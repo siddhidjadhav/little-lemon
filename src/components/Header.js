ce76cc8cb37c88f7c5a655c756cfc560b1067e8d
siddhi jadhav <siddhidilipjadhav2000@gmail.com>
2025-03-21 11:13:37 -0700

    final final commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import bannerImg from '../images/restauranfood.jpg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Header=()=>{
+const Header = () => {
   return (
-   <header className="header">
+    <header className="header">
+      <section>
+        <div className="banner">
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>
+            We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+          </p>
+          <Link to="./booking">
+            <button aria-label="On Click">Reserve Table</button>
+          </Link>
+          
+        </div>
 
-        <section>
-            <div className="banner">
-                <h2>Little Lemon</h2>
-                <h3>Chicago</h3>
-                <p>We are family owned Mediterraneran restraurant, focused on traditional recepies served with modern twist.</p>
-                <Link to="./booking"><button aria-Label='On Click'>Reserve Table</button></Link>
-            </div>
-
-            <div className="banner-img">
-                <img src="bannerImg" alt="" />
-            </div>
-        </section>
-   </header>
+        <div className="banner-img">
+          <img src={bannerImg} alt="Restaurant Food"style={{ width: '300px', height: '300px' }} />
+        </div>
+      </section>
+    </header>
   );
-}
+};
 
 export default Header;
